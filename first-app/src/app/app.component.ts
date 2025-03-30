097a0ce7b19a3a72f03e7d2c19e829177fe4614d
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-03-30 21:33:00 +0800

    feat(first-app): integrate header to root app, with styling

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [HomeComponent, HeaderComponent],
+  template: `
+    <app-header />
+    <main class="p-4">
+      <app-home />
+    </main>
+  `,
 })
 export class AppComponent {
   title = 'first-app';
