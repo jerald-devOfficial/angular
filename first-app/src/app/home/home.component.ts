e13d7b091172f219ea23207be76c7f6291faa1af
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-03-30 23:08:42 +0800

    feat(first-app): create counter and greeting component, integrate passing data to other component and try event listener

--- first-app/src/app/home/home.component.ts
+++ first-app/src/app/home/home.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { GreetingComponent } from '../components/greeting/greeting.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [GreetingComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  homeMessage = signal('Hello, World!');
 
+  keyUpHandler(event: KeyboardEvent) {
+    console.log('user pressed', event.key);
+  }
 }
