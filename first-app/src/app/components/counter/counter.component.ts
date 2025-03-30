a2505eca2c7b0abb78f8fcbd9a8be69ab87dcb57
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-03-30 23:15:47 +0800

    feat(first-app): implement counter buttons with functionalities

--- first-app/src/app/components/counter/counter.component.ts
+++ first-app/src/app/components/counter/counter.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   imports: [],
   templateUrl: './counter.component.html',
-  styleUrl: './counter.component.css'
+  styleUrl: './counter.component.css',
 })
 export class CounterComponent {
+  counterValue = signal(0);
+  increment() {
+    this.counterValue.update((value) => value + 1);
+  }
 
+  decrement() {
+    this.counterValue.update((value) => value - 1);
+  }
+
+  reset() {
+    this.counterValue.set(0);
+  }
 }
--- first-app/src/app/home/home.component.ts
+++ first-app/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { CounterComponent } from '../components/counter/counter.component';
 import { GreetingComponent } from '../components/greeting/greeting.component';
 
 @Component({
   selector: 'app-home',
-  imports: [GreetingComponent],
+  imports: [GreetingComponent, CounterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
