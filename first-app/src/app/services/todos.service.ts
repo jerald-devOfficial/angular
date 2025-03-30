bbc1c1f1bbff06db1f6c7b4958459e2be5f059c6
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-03-30 23:45:29 +0800

    feat(first-app): implement angular service in todos component and loop display of mock todos

--- first-app/src/app/services/todos.service.ts
+++ first-app/src/app/services/todos.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Todo } from '../model/todo.type';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodosService {
-
-  constructor() { }
+  todoItems: Todo[] = [
+    {
+      userId: 1,
+      id: 1,
+      title: 'Buy groceries',
+      completed: false,
+    },
+    {
+      userId: 1,
+      id: 2,
+      title: 'Buy milk',
+      completed: false,
+    },
+  ];
+  constructor() {}
 }
--- first-app/src/app/todos/todos.component.ts
+++ first-app/src/app/todos/todos.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Todo } from '../model/todo.type';
+import { TodosService } from '../services/todos.service';
 @Component({
   selector: 'app-todos',
   imports: [],
   templateUrl: './todos.component.html',
-  styleUrl: './todos.component.css'
+  styleUrl: './todos.component.css',
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit {
+  todoService = inject(TodosService);
+  todoItems = signal<Todo[]>([]);
 
+  ngOnInit(): void {
+    console.log(this.todoService.todoItems);
+    this.todoItems.set(this.todoService.todoItems);
+  }
 }
